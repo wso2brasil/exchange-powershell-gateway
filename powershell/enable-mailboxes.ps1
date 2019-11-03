85ebe99bb35692a80cb3f3273775eefe95276a63
Joao Emilio <joaoemilio@gmail.com>
2019-11-03 11:29:53 -0300

    release candidate to enable mailboxes

--- powershell/enable-mailboxes.ps1
+++ powershell/enable-mailboxes.ps1
@@ -1,63 +1,29 @@
-$config = Get-Content 'config.json' | Out-String | ConvertFrom-Json
+<# import some auxiliary functions #>
+. .\functions.ps1
 
-$path = $config.path_enable_in + "\*.json"
-$timestamp = Get-Date -Format 'yyyy-MM-dd_hh-mm-ss'   
+$global:config = loadConfigVariables
+createOutFolders
 
-function Write-Log {
-    Param( $Message )
-    $logFile = $config.path_logs + "enable-mailboxes-" + $timestamp + ".log"
-    Write-Output $logFile
-    if ( -not (Test-Path -Path $config.path_logs -PathType Container) ) {
-        New-Item -ItemType "directory" -Path $config.path_logs
-    }
+$global:timestamp = Get-Date -Format 'yyyy-MM-dd_hh-mm-ss'
+$global:logFile = $global:config.path_logs + "enable-mailboxes-" + $global:timestamp + ".log"
 
-    $timestamp = Get-Date -Format 'yyyy-MM-dd hh:mm:ss'
-
-    "[$timestamp] $Message" | Tee-Object -FilePath $logFile -Append | Write-Verbose
+$files = loadFiles
+if ([string]::IsNullOrEmpty($files)) {
+    Write-Log "No mailboxes to enable"
+    return;
 }
 
-Write-Log "Retrieving requests"
-$files = @(Get-ChildItem -Path $path)
 if($files.count -lt 1) {
     Write-Log "No mailboxes to enable"
     return;
 }
 
+startPSSession
+validatePSSession
+
 Write-Log "$($files.count) files to process"
-Write-Log "Creating PowerShell Session"
-$opt = New-PSSessionOption -SkipCACheck -SkipCNCheck
-#$s = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $config.exchange_server_uri -Credential $config.admin -Authentication Digest -SessionOption $opt -AllowRedirection
-Write-Log "Importing PowerShell Session"
-Import-PSSession $s
-Write-Log "Validating PowerShell Session"
-$validation = Get-Mailbox 
-Write-Output $validation 
-if ( -not (Test-Path -Path $config.path_enable_out -PathType Container) ) {
-    New-Item -ItemType "directory" -Path $config.path_enable_out
-}
-
-if ( -not (Test-Path -Path $config.path_disable_out -PathType Container) ) {
-    New-Item -ItemType "directory" -Path $config.path_disable_out
-}
-
 foreach ($element in $files) {
-    $json = Get-Content $element | Out-String | ConvertFrom-Json
-    Write-Log $element.name 
-
-    if (-not ([string]::IsNullOrEmpty($json.identity))) {
-        $identity = Get-Mailbox $json.identity 
-        #verify if identity exists    
-        $alias = write-output ($identity | Select -ExpandProperty "Alias")
-        if (-not ([string]::IsNullOrEmpty($alias))) {
-            Write-Log $alias
-        } else {
-            Write-Log "There isn't a mailbox associated"
-            $enable = Enable-Mailbox $json.identity 
-        }
-    } else {
-        Write-Log "json.identity is null or empty"
-    }
-    Move-Item -Path $element -Destination $config.path_enable_out
-
+    $filePath = $global:config.path_enable_in + "\" + $element
+    enableMailbox $filePath
 }
 
---
+++ powershell/functions.ps1
@@ -0,0 +1,74 @@
+function loadConfigVariables {    
+    <# load config file #>
+    $_config = Get-Content 'config.json' | Out-String | ConvertFrom-Json
+    return $_config
+}
+
+function createOutFolders {    
+    <# create folders to move files processed if they don't exist #>
+    if ( -not (Test-Path -Path $global:config.path_enable_out -PathType Container) ) {
+        New-Item -ItemType "directory" -Path $global:config.path_enable_out
+    }
+
+    if ( -not (Test-Path -Path $global:config.path_disable_out -PathType Container) ) {
+        New-Item -ItemType "directory" -Path $global:config.path_disable_out
+    }
+
+    if ( -not (Test-Path -Path $global:config.path_logs -PathType Container) ) {
+        New-Item -ItemType "directory" -Path $global:config.path_logs
+    }
+
+}
+
+function loadFiles {
+    Write-Log "Retrieving requests"
+    $path = $global:config.path_enable_in
+    $files = @(Get-ChildItem -Path $path)
+    return $files
+}
+
+function startPSSession {
+    <# Initiate session to start processing #>
+    $opt = New-PSSessionOption -SkipCACheck -SkipCNCheck
+    $s = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $global:config.exchange_server_uri -Credential $global:config.admin -Authentication Digest -SessionOption $opt -AllowRedirection
+
+    Write-Log $global:config "Importing PowerShell Session"
+    Import-PSSession $s    
+}
+
+function validatePSSession {
+    Write-Log "Validating PowerShell Session"
+    $validation = Get-Mailbox joaoemilio
+    Write-Output $validation     
+}
+
+function enableMailbox( $file ) {
+    $json = Get-Content $file | Out-String | ConvertFrom-Json
+    Write-Log $file
+
+    if (-not ([string]::IsNullOrEmpty($json.identity))) {
+        $identity = Get-Mailbox $json.identity 
+        #verify if identity exists    
+        $alias = write-output ($identity | Select -ExpandProperty "Alias")
+        if (-not ([string]::IsNullOrEmpty($alias))) {
+            $msg = $alias + " already has a mailbox enabled"
+            Write-Log $msg
+        } else {
+            $msg = "Enabling mailbox for " + $json.identity
+            Write-Log $msg 
+            $enable = Enable-Mailbox $json.identity 
+            Write-Log $enable 
+        }
+    } else {
+        Write-Log "json.identity is null or empty"
+    }
+    Move-Item -Path $file -Destination $global:config.path_enable_out
+}
+
+
+<#
+Auxiliary function to write log messages 
+#>
+function Write-Log ( $Message ) {
+    "[$timestamp] $Message" | Tee-Object -FilePath $global:logFile -Append | Write-Verbose
+}
